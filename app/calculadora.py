fb897a078b762d962134401c4f19a3eaf80c8e67
Nicolas Peña <nicolaspj999@gmail.com>
2026-04-18 12:42:51 -0500

    configuración inicial

---
+++ app/calculadora.py
@@ -0,0 +1,27 @@
+"""Módulo con funciones básicas de cálculo."""
+
+# app/calculadora.py
+
+AUTORES = "npenaj, mquijanoj, dcmendozab, vjvilladia"
+
+
+def sumar(a, b):
+    """Suma dos números."""
+    return a + b
+
+
+def restar(a, b):
+    """Resta dos números."""
+    return a - b
+
+
+def multiplicar(a, b):
+    """Multiplica dos números."""
+    return a * b
+
+
+def dividir(a, b):
+    """Divide dos números."""
+    if b == 0:
+        raise ZeroDivisionError("No se puede dividir por cero")
+    return a / b
